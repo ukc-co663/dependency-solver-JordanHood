f83118e54bd01d63776e7cedf31b1e9aba7c3d6e
Jordan Hood <jordan.hood@virginmedia.com>
2018-01-22 22:30:03 +0000

    base ideas

--- main.js
+++ main.js
@@ -1,5 +1,54 @@
+const repository = require('./' + process.argv[2])
+// const initial = require('./' + process.argv[3])
+// const constraints = require('./' + process.argv[4])
 
-const repositry = require('./' + process.argv[2])
-// const initial = require('./' + process.argv[2])
-// const repositry = require('./' + process.argv[2])
-console.log(repositry)
+class Dependency {
+  constructor (name) {
+    this.name = name
+  }
+}
+
+class Package {
+  constructor (name, version, size, dependencies, conflicts){
+    this.name = name
+    this.version = version
+    this.size = size
+    this.conflicts = conflicts
+    this.buildDependencies(dependencies)
+  }
+
+  buildDependencies (dependencies) {
+    this.dependencies = dependencies.map(dependency => {
+      const deps = []
+      for (let dep of dependency) {
+        deps.push(new Dependency(dep))
+      }
+      return deps
+    })
+  }
+}
+
+class Repository {
+  constructor (packages) {
+    this.packages = {}
+    this.buildPackages(packages)
+    }
+
+    buildPackages (packages){
+      for(let pkg of packages) {
+        const {
+           name,
+           version,
+           size,
+           depends = [],
+           conflicts = []
+         } = pkg
+         let pkgs = new Package(name, version, size, depends, conflicts)
+         this.packages[pkgs.name + '@' + pkgs.version] = pkgs
+    }
+  }
+}
+
+const repo = new Repository(repository)
+
+console.log(repo.packages)

6eab0a588b1c1eb4dbc5ebc665a88736935ec6db
Jordan Hood <jordan.hood@virginmedia.com>
2018-01-24 21:19:09 +0000

    constraints done and started building the deps graph

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-const repository = require('./' + process.argv[2])
-const initial = require('./' + process.argv[3])
-// const constraints = require('./' + process.argv[4])
 const Repository = require('./lib/repository')
+const repository = new Repository(require('./' + process.argv[2]))
+const constraints = require('./' + process.argv[4])
+const installer = require('./lib/installer')
+// read constraints and build up base dependancy tree object
+// traverse dependancy tree looking for package conflicts and dependayies
+// build and return comands array
 
-const repo = new Repository(repository)
-
+return installer.execute({repository, constraints})

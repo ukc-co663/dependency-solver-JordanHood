82cfdb747805e1ccef83c79ab3c6af4d606318b8
Jordan Hood <jordan.hood@virginmedia.com>
2018-01-24 16:50:09 +0000

    repo structure done

--- main.js
+++ main.js
@@ -1,60 +1,7 @@
 const repository = require('./' + process.argv[2])
-// const initial = require('./' + process.argv[3])
+const initial = require('./' + process.argv[3])
 // const constraints = require('./' + process.argv[4])
-
-class Dependency {
-  constructor (name) {
-    this.name = name
-  }
-}
-
-class Package {
-  constructor (name, version, size, dependencies, conflicts) {
-    this.name = name
-    this.version = version
-    this.size = size
-    this.conflicts = conflicts
-    this.buildDependencies(dependencies)
-  }
-
-  buildDependencies (dependencies) {
-    this.dependencies = dependencies.map(dependency => {
-      const deps = []
-      for (let dep of dependency) {
-        deps.push(new Dependency(dep))
-      }
-      return deps
-    })
-  }
-}
-
-class Repository {
-  constructor (packages) {
-    this.packages = {}
-    this.buildPackages(packages)
-  }
-
-  buildPackages (packages) {
-    for (let pkg of packages) {
-      const {
-          name,
-          version,
-          size,
-          depends = [],
-          conflicts = []
-        } = pkg
-      let pkgs = new Package(name, version, size, depends, conflicts)
-      if (!this.packages[pkgs.name]) {
-        this.packages[pkgs.name] = {
-          [pkgs.version]: pkgs
-        }
-      } else {
-        this.packages[pkgs.name][pkgs.version] = pkgs
-      }
-    }
-  }
-}
+const Repository = require('./lib/repository')
 
 const repo = new Repository(repository)
 
-console.log(repo.packages)

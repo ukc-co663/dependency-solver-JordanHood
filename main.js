95508ca53e9caafd0c26843fa5d5a181fd25bc63
Jordan Hood <jordan.hood@virginmedia.com>
2018-01-31 17:03:32 +0000

    tidy up

--- main.js
+++ main.js
@@ -2,12 +2,9 @@ const Repository = require('./lib/repository')
 const repository = new Repository(require('./' + process.argv[2]))
 const constraints = require('./' + process.argv[4])
 const installer = require('./lib/installer')
+
 // read constraints and build up base dependancy tree object
 // traverse dependancy tree looking for package conflicts and dependayies
 // build and return comands array
 
-try {
-  installer.execute({repository, constraints})
-} catch (e) {
-  console.error(e)
-}
+console.log(installer.execute({repository, constraints}))

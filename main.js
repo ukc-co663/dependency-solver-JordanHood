94a8d749f7c12406869166abdcce4f04bd834434
Jordan Hood <jordan.hood@virginmedia.com>
2018-01-31 17:13:25 +0000

    test

--- main.js
+++ main.js
@@ -3,8 +3,16 @@ const repository = new Repository(require('./' + process.argv[2]))
 const constraints = require('./' + process.argv[4])
 const installer = require('./lib/installer')
 
-// read constraints and build up base dependancy tree object
-// traverse dependancy tree looking for package conflicts and dependayies
-// build and return comands array
+// check that the dep exists In the repo, as well as the version 
+// get competitors working on constraints
+// build and check the initial json for what is installed and alter as such
 
+// and test like hell
+
+// and deal with conflict managment
+
+// c.f. man, on init if conflicts go through each package deps and pick out conflicts, compare to main pkg deps and resolve
+
+// cyclic deps need to lookino
 console.log(installer.execute({repository, constraints}))
+

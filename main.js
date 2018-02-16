8ebb3286a65d76f27bf5d49641656cebd58e333f
Jordan Hood <jordan.hood@virginmedia.com>
2018-02-16 21:16:16 +0000

    dont add comands that have been added

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 const Repository = require('./lib/repository')
 const repository = new Repository(require('./' + process.argv[2]))
-debugger
 // console.log('repository', repository.packages.A['2.01'])
 
 const constraints = require('./' + process.argv[4])
 const installer = require('./lib/installer')
 const fs = require('fs')
 
-// check that the dep exists In the repo, as well as the version 
+// check that the dep exists In the repo, as well as the version
 // get competitors working on constraints
 // build and check the initial json for what is installed and alter as such
 
@@ -19,7 +18,7 @@ const fs = require('fs')
 
 // cyclic deps need to lookino
 const result = installer.execute({repository, constraints})
-console.log('fin', result)
+// console.log('fin', result)
 fs.writeFile('./commands.json', JSON.stringify(result.commands), err => {
   if (err) return console.log(err)
 })

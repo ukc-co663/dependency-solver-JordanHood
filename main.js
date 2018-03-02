5b9df85b8eab477fd1d88a9ff890219e2f025bc3
Jordan Hood <jordan.hood@virginmedia.com>
2018-03-02 08:54:49 +0000

    log commands to console in json format

--- main.js
+++ main.js
@@ -4,6 +4,4 @@ const constraints = require('./' + process.argv[4])
 const installer = require('./lib/installer')
 const fs = require('fs')
 const result = installer.execute({repository, constraints})
-fs.writeFile('./commands.json', JSON.stringify(result.commands), err => {
-  if (err) return console.error(err)
-})
+console.log('%j', result.commands)
